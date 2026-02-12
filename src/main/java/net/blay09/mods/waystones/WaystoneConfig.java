a72776e33e15a02e959ff118b7f8bfd8e82505eb
Guilherme Zaluchi <dev@guilherme.zaluchi.com.br>
2026-02-11 23:15:58 -0300

    Added (client-side only) custom text formatting for the name of global waystones. Only affects the list of warps for now.

--- src/main/java/net/blay09/mods/waystones/WaystoneConfig.java
+++ src/main/java/net/blay09/mods/waystones/WaystoneConfig.java
@@ -28,43 +28,70 @@ public class WaystoneConfig {
 	public boolean interDimension;
 	public boolean privateWaystones;
 
-
 	public boolean creativeModeOnly;
 	public boolean setSpawnPoint;
 
 	public boolean globalNoCooldown;
 	public boolean globalInterDimension;
 
+	// Custom formating for global waystones.
+	// If unset, simply uses Yellow color.
+	// Only affects the list of warps.
+	// TODO: Affect chat activation message.
+	public static String globalNameFormating;
+
 	public void reloadLocal(Configuration config) {
-		teleportButton = config.getBoolean("Teleport Button in GUI", "general", false, "Should there be a button in the inventory to access the waystone menu?");
-		teleportButtonCooldown = config.getInt("Teleport Button Cooldown", "general", 300, 0, 86400, "The cooldown between usages of the teleport button in seconds.");
-		teleportButtonReturnOnly = config.getBoolean("Teleport Button Return Only", "general", false, "If true, the teleport button will only let you return to the last activated waystone, instead of allowing to choose.");
+		teleportButton = config.getBoolean("Teleport Button in GUI", "general", false,
+				"Should there be a button in the inventory to access the waystone menu?");
+		teleportButtonCooldown = config.getInt("Teleport Button Cooldown", "general", 300, 0, 86400,
+				"The cooldown between usages of the teleport button in seconds.");
+		teleportButtonReturnOnly = config.getBoolean("Teleport Button Return Only", "general", false,
+				"If true, the teleport button will only let you return to the last activated waystone, instead of allowing to choose.");
 
-		allowReturnScrolls = config.getBoolean("Allow Return Scrolls", "general", true, "If true, return scrolls will be craftable.");
-		lootReturnScrolls = config.getBoolean("Spawn Return Scrolls as loot", "general", true, "If true, return scrolls will be found in dungeons as loot.");
-		allowWarpStone = config.getBoolean("Allow Warp Stone", "general", true, "If true, the warp stone will be craftable.");
+		allowReturnScrolls = config.getBoolean("Allow Return Scrolls", "general", true,
+				"If true, return scrolls will be craftable.");
+		lootReturnScrolls = config.getBoolean("Spawn Return Scrolls as loot", "general", true,
+				"If true, return scrolls will be found in dungeons as loot.");
+		allowWarpStone = config.getBoolean("Allow Warp Stone", "general", true,
+				"If true, the warp stone will be craftable.");
 
-		teleportButtonX = config.getInt("Teleport Button GUI X", "client", 60, -100, 250, "The x position of the warp button in the inventory.");
-		teleportButtonY = config.getInt("Teleport Button GUI Y", "client", 60, -100, 250, "The y position of the warp button in the inventory.");
-		disableTextGlow = config.getBoolean("Disable Text Glow", "client", false, "If true, the text overlay on waystones will no longer always render at full brightness.");
-		disableParticles = config.getBoolean("Disable Particles", "client", false, "If true, activated waystones will not emit particles.");
+		teleportButtonX = config.getInt("Teleport Button GUI X", "client", 60, -100, 250,
+				"The x position of the warp button in the inventory.");
+		teleportButtonY = config.getInt("Teleport Button GUI Y", "client", 60, -100, 250,
+				"The y position of the warp button in the inventory.");
+		disableTextGlow = config.getBoolean("Disable Text Glow", "client", false,
+				"If true, the text overlay on waystones will no longer always render at full brightness.");
+		disableParticles = config.getBoolean("Disable Particles", "client", false,
+				"If true, activated waystones will not emit particles.");
 
-		warpStoneCooldown = config.getInt("Warp Stone Cooldown", "general", 300, 0, 86400, "The cooldown between usages of the warp stone in seconds.");
+		warpStoneCooldown = config.getInt("Warp Stone Cooldown", "general", 300, 0, 86400,
+				"The cooldown between usages of the warp stone in seconds.");
 
-		setSpawnPoint = config.getBoolean("Set Spawnpoint on Activation", "general", false, "If true, the player's spawnpoint will be set to the last activated waystone.");
-		interDimension = config.getBoolean("Interdimensional Teleport", "general", true, "If true, all waystones work inter-dimensionally.");
-		privateWaystones = config.getBoolean("Private Waystones", "general", false, "If true, only owners can break their own waystones.");
+		setSpawnPoint = config.getBoolean("Set Spawnpoint on Activation", "general", false,
+				"If true, the player's spawnpoint will be set to the last activated waystone.");
+		interDimension = config.getBoolean("Interdimensional Teleport", "general", true,
+				"If true, all waystones work inter-dimensionally.");
+		privateWaystones = config.getBoolean("Private Waystones", "general", false,
+				"If true, only owners can break their own waystones.");
 
-		creativeModeOnly = config.getBoolean("Creative Mode Only", "general", false, "If true, waystones can only be placed in creative mode.");
+		creativeModeOnly = config.getBoolean("Creative Mode Only", "general", false,
+				"If true, waystones can only be placed in creative mode.");
 
-		globalNoCooldown = config.getBoolean("No Cooldown on Global Waystones", "general", true, "If true, waystones marked as global have no cooldown.");
-		globalInterDimension = config.getBoolean("Interdimensional Teleport on Global Waystones", "general", true, "If true, waystones marked as global work inter-dimensionally.");
+		globalNoCooldown = config.getBoolean("No Cooldown on Global Waystones", "general", true,
+				"If true, waystones marked as global have no cooldown.");
+		globalInterDimension = config.getBoolean("Interdimensional Teleport on Global Waystones", "general", true,
+				"If true, waystones marked as global work inter-dimensionally.");
 
-		String[] serverWaystoneData = config.getStringList("Server Waystones", "generated", new String[0], "This option is automatically populated by the server when using the Server Hub Mode. Do not change.");
+		globalNameFormating = config.getString("Custom text formating for Global Waystones", "general",
+				"\u00a7e",
+				"Custom formating for the name of global waystones. Leave empty for default (yellow text).");
+
+		String[] serverWaystoneData = config.getStringList("Server Waystones", "generated", new String[0],
+				"This option is automatically populated by the server when using the Server Hub Mode. Do not change.");
 		WaystoneEntry[] serverWaystones = new WaystoneEntry[serverWaystoneData.length];
-		for(int i = 0; i < serverWaystones.length; i++) {
+		for (int i = 0; i < serverWaystones.length; i++) {
 			String[] split = serverWaystoneData[i].split("\u00a7");
-			if(split.length < 3) {
+			if (split.length < 3) {
 				serverWaystones[i] = new WaystoneEntry("Invalid Waystone", 0, new BlockPos(0, 0, 0));
 				serverWaystones[i].setGlobal(true);
 				continue;
@@ -72,7 +99,8 @@ public class WaystoneConfig {
 			try {
 				int dimensionId = Integer.parseInt(split[1]);
 				String[] pos = split[2].split(",");
-				serverWaystones[i] = new WaystoneEntry(split[0], dimensionId, new BlockPos(Integer.parseInt(pos[0]), Integer.parseInt(pos[1]), Integer.parseInt(pos[2])));
+				serverWaystones[i] = new WaystoneEntry(split[0], dimensionId,
+						new BlockPos(Integer.parseInt(pos[0]), Integer.parseInt(pos[1]), Integer.parseInt(pos[2])));
 				serverWaystones[i].setGlobal(true);
 			} catch (NumberFormatException e) {
 				serverWaystones[i] = new WaystoneEntry("Invalid Waystone", 0, new BlockPos(0, 0, 0));
@@ -85,12 +113,15 @@ public class WaystoneConfig {
 	public static void storeServerWaystones(Configuration config, Collection<WaystoneEntry> entries) {
 		String[] serverWaystones = new String[entries.size()];
 		int i = 0;
-		for(WaystoneEntry entry : entries) {
-			serverWaystones[i] = entry.getName() + "\u00a7" + entry.getDimensionId() + "\u00a7" + entry.getPos().getX() + "," + entry.getPos().getY() + "," + entry.getPos().getZ();
+		for (WaystoneEntry entry : entries) {
+			serverWaystones[i] = entry.getName() + "\u00a7" + entry.getDimensionId() + "\u00a7" + entry.getPos().getX()
+					+ "," + entry.getPos().getY() + "," + entry.getPos().getZ();
 			i++;
 		}
-		config.get("generated", "Server Waystones", "This option is automatically populated by the server when using the Server Hub Mode. Do not change.").set(serverWaystones);
-		if(config.hasChanged()) {
+		config.get("generated", "Server Waystones",
+				"This option is automatically populated by the server when using the Server Hub Mode. Do not change.")
+				.set(serverWaystones);
+		if (config.hasChanged()) {
 			config.save();
 		}
 	}
--- src/main/java/net/blay09/mods/waystones/client/gui/GuiButtonWaystone.java
+++ src/main/java/net/blay09/mods/waystones/client/gui/GuiButtonWaystone.java
@@ -1,5 +1,6 @@
 package net.blay09.mods.waystones.client.gui;
 
+import net.blay09.mods.waystones.WaystoneConfig;
 import net.blay09.mods.waystones.util.WaystoneEntry;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.util.EnumChatFormatting;
@@ -9,7 +10,7 @@ public class GuiButtonWaystone extends GuiButton {
 	private final WaystoneEntry waystone;
 
 	public GuiButtonWaystone(int id, int x, int y, WaystoneEntry waystone) {
-		super(id, x, y, (waystone.isGlobal() ? EnumChatFormatting.YELLOW : "") + waystone.getName());
+		super(id, x, y, (waystone.isGlobal() ? WaystoneConfig.globalNameFormating : "") + waystone.getName());
 		this.waystone = waystone;
 	}
 
