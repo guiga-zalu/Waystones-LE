09791fdf63b16816a3589649b4842568e6704d1f
Guilherme Zaluchi <dev@guilherme.zaluchi.com.br>
2026-02-12 21:02:09 -0300

    Before testing the math.

--- src/main/java/net/blay09/mods/waystones/client/gui/GuiWarpStone.java
+++ src/main/java/net/blay09/mods/waystones/client/gui/GuiWarpStone.java
@@ -16,7 +16,9 @@ import java.util.Iterator;
 
 public class GuiWarpStone extends GuiScreen {
 
+	private static final int BUTTON_WIDTH = 95;
 	private static final int BUTTON_HEIGHT = 22;
+	private static int buttonsPerPage;
 	private final WaystoneEntry[] entries;
 	private GuiButton btnPrevPage;
 	private GuiButton btnNextPage;
@@ -31,13 +33,20 @@ public class GuiWarpStone extends GuiScreen {
 	@Override
 	@SuppressWarnings("unchecked")
 	public void initGui() {
+		buttonsPerPage = WaystoneConfig.warpListButtonsPerPage;
+
 		// TODO: recalculate positions based on resolution and buttons per page.
-		btnPrevPage = new GuiButton(0, width / 2 - 100, height / 2 + 40, 95, 20,
+		int centerX = width / 2;
+		int centerY = height / 2;
+		int prevX = centerX - 100;
+		int prevY = centerY + 40;
+		btnPrevPage = new GuiButton(0, prevX, prevY, BUTTON_WIDTH, BUTTON_HEIGHT,
 				I18n.format("gui.waystones:warpStone.previousPage"));
 		buttonList.add(btnPrevPage);
 
-		// TODO: recalculate.
-		btnNextPage = new GuiButton(1, width / 2 + 5, height / 2 + 40, 95, 20,
+		int nextX = centerX + 5;
+		int nextY = prevY;
+		btnNextPage = new GuiButton(1, nextX, nextY, BUTTON_WIDTH, BUTTON_HEIGHT,
 				I18n.format("gui.waystones:warpStone.nextPage"));
 		buttonList.add(btnNextPage);
 
@@ -46,8 +55,6 @@ public class GuiWarpStone extends GuiScreen {
 
 	@SuppressWarnings("unchecked")
 	public void updateList() {
-		int buttonsPerPage = WaystoneConfig.warpListButtonsPerPage;
-
 		btnPrevPage.enabled = pageOffset > 0;
 		btnNextPage.enabled = pageOffset < (entries.length - 1) / buttonsPerPage;
 
@@ -58,6 +65,17 @@ public class GuiWarpStone extends GuiScreen {
 			}
 		}
 
+		int btnInitialX = width / 2 - 100;
+		/**
+		 * | per page | title | initialY | pagingY |
+		 * | .....? 1 | . -52 | .... -27 | ... +07 |
+		 * | .....? 2 | . -63 | .... -38 | ... +18 |
+		 * | .....= 3 | . -74 | .... -49 | ... +29 |
+		 * | ...... 4 | . -85 | .... -60 | ... +40 |
+		 * | .....= 5 | . -96 | .... -71 | ... +51 |
+		 */
+		int btnInitialY = height / 2 - 60;
+
 		int y = 0;
 		int currDimensionId = Minecraft.getMinecraft().theWorld.provider.dimensionId;
 		for (int i = 0; i < buttonsPerPage; i++) {
@@ -65,15 +83,13 @@ public class GuiWarpStone extends GuiScreen {
 			if (entryIndex < 0 || entryIndex >= entries.length)
 				continue;
 
-			// TODO: recalculate.
-			GuiButtonWaystone btnWaystone = new GuiButtonWaystone(
-					2 + i, width / 2 - 100, height / 2 - 60 + y,
-					entries[entryIndex]);
-			if (entries[entryIndex].getDimensionId() != currDimensionId) {
-				btnWaystone.enabled = WaystoneManager.isDimensionWarpAllowed(entries[entryIndex]);
+			WaystoneEntry entry = entries[entryIndex];
+			GuiButtonWaystone btnWaystone = new GuiButtonWaystone(2 + i, btnInitialX, btnInitialY + y, entry);
+			if (entry.getDimensionId() != currDimensionId) {
+				btnWaystone.enabled = WaystoneManager.isDimensionWarpAllowed(entry);
 			}
 			buttonList.add(btnWaystone);
-			y += BUTTON_HEIGHT;
+			y += BUTTON_HEIGHT + 2;
 		}
 	}
 
@@ -97,9 +113,18 @@ public class GuiWarpStone extends GuiScreen {
 		drawWorldBackground(0);
 		super.drawScreen(mouseX, mouseY, partialTicks);
 		GL11.glColor4f(1f, 1f, 1f, 1f);
-		drawRect(width / 2 - 50, height / 2 - 50, width / 2 + 50, height / 2 + 50, 0xFFFFFF);
-		drawCenteredString(fontRendererObj, I18n.format("gui.waystones:warpStone.selectDestination"), width / 2,
-				height / 2 - 85, 0xFFFFFF);
+
+		int centerX = width / 2;
+		int centerY = height / 2;
+		int rectSize = 100;
+		int rectSize_2 = rectSize / 2;
+
+		drawRect(centerX - rectSize_2, centerY - rectSize_2, centerX + rectSize_2, centerY + rectSize_2, 0xFFFFFF);
+
+		int titleY = centerY - 85;
+		drawCenteredString(
+				fontRendererObj, I18n.format("gui.waystones:warpStone.selectDestination"),
+				centerX, titleY, 0xFFFFFF);
 	}
 
 }
